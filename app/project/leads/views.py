1dac39ede4a05ddc8de35ab5f6216e33eac1b649
Jacob Muchow <jacobmuchow@quarkworks.co>
2018-11-16 09:07:29 -0600

    Hooked up first API endpoint

--- app/project/leads/views.py
+++ app/project/leads/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from leads.models import Lead
+from leads.serializers import LeadSerializer
+
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
